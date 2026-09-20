3acc1c43ad1126548995b804945655995c7a0e1d
Gabriel <166170970+Fanxy13@users.noreply.github.com>
2026-09-20 21:16:22 +0200

    Add ramp: Hex digits (#43)
    17 steps

--- js/presets.js
+++ js/presets.js
@@ -34,7 +34,8 @@ const RAMPS = [
   { name: "Diamonds",     chars: " \u00b7\u22c4\u25c7\u25c8\u25c6\u2666" },
   { name: "Stars",        chars: " \u00b7\u02da\u2726\u2727\u2605\u2736\u2739" },
   { name: "Plus grid",    chars: " .\u00b7+\u2020\u2021#\u2593" },
-  { name: "Slashes",      chars: " .:/|\\X\u2573\u2588" }
+  { name: "Slashes",      chars: " .:/|\\X\u2573\u2588" },
+  { name: "Hex digits",   chars: " 0123456789ABCDEF" }
 ];
 
 const PALETTES = [

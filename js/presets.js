e032e3dbabc835793c598a19f85afb2c1fd51323
Gabriel <166170970+Fanxy13@users.noreply.github.com>
2026-09-20 21:36:29 +0200

    Add ramp: Fractions (#118)
    9 steps

--- js/presets.js
+++ js/presets.js
@@ -59,7 +59,8 @@ const RAMPS = [
   { name: "Weather",      chars: " .\u2601\u2602\u2603\u2605\u2600" },
   { name: "Zodiac",       chars: " .\u264f\u2653\u264b\u2650\u2652\u2651" },
   { name: "Runes",        chars: " .\u16a0\u16a2\u16b1\u16b7\u16c1\u16d7\u16dd" },
-  { name: "Superscript",  chars: " \u00b9\u00b2\u00b3\u2074\u2075\u2076\u2077\u2078\u2079\u2070" }
+  { name: "Superscript",  chars: " \u00b9\u00b2\u00b3\u2074\u2075\u2076\u2077\u2078\u2079\u2070" },
+  { name: "Fractions",    chars: " \u215b\u00bc\u2153\u00bd\u2154\u00be\u215e1" }
 ];
 
 const PALETTES = [

ded66a33844cd336443e8e52f3fcc9924fc7de44
Gabriel <166170970+Fanxy13@users.noreply.github.com>
2026-09-20 21:30:39 +0200

    Add palette: Cotton Candy (#95)
    5 steps, dark background

--- js/presets.js
+++ js/presets.js
@@ -123,7 +123,8 @@ const PALETTES = [
   { name: "Cyber",           colors: ["#0a0e17", "#1b3a4b", "#00d9ff", "#ff2a6d", "#d1f7ff"], bg: "#05080d" },
   { name: "Acid",            colors: ["#0f1a00", "#2e4d00", "#6b9e00", "#aaff00", "#e0ffb3"], bg: "#080d00" },
   { name: "Toxic",           colors: ["#0a1400", "#2b4d0a", "#5c9e14", "#9eff1f", "#d9ffa8"], bg: "#050a00" },
-  { name: "Bubblegum",       colors: ["#3d1a35", "#7a2f66", "#c44f9e", "#ff85c8", "#ffd6ed"], bg: "#1f0d1b" }
+  { name: "Bubblegum",       colors: ["#3d1a35", "#7a2f66", "#c44f9e", "#ff85c8", "#ffd6ed"], bg: "#1f0d1b" },
+  { name: "Cotton Candy",    colors: ["#2e2447", "#5c4b8a", "#9b8ce0", "#ffb3d9", "#fff0f7"], bg: "#171224" }
 ];
 
 if (typeof module !== "undefined" && module.exports) {
